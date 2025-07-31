cc5726e58c7e8766bcb037273d331ae0ff496241
e-mandy <andymfrd02@gmail.com>
2025-07-31 22:09:42 +0100

    Mise en place de la fonction de calcul d'entropie

---
+++ main.py
@@ -0,0 +1,3 @@
+from src.utils import calculer_entropie_shannon
+
+print(calculer_entropie_shannon())
--- src/utils.py
+++ src/utils.py
@@ -1,2 +1,17 @@
+import math
+
 def valider_texte_dechiffre(texte):
     pass
+
+def calculer_entropie_shannon(data):
+   bytes = bytes(data)
+   entropie = 0
+   proba_byte = 0
+   for specifique_byte in bytes:
+       i = 1
+       for chaque_byte in bytes:
+            if(chaque_byte == specifique_byte):
+                i += 1
+       proba_byte = 1 / i
+       entropie +=  (proba_byte) * math.log(1/proba_byte, 8)
+   return entropie
