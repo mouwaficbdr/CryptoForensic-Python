8c0bd17a865ab94b8377046d07bfe10d48960d88
DevLawer <ogoudedjimonde@gmail.com>
2025-08-04 08:47:24 +0100

    Implémentation de la fonction de recherche d'un rapport de mission (#15)
    * Implementation de la fonction de validation du texte déchiffrer
    
    * Définition de la classe de gestion des rapports de ission et implémentatioon de la fonction de génération des rapport de synthèse
    
    * Implémentation de la fonction de recherche d'anciens rapports
    
    ---------
    
    Co-authored-by: Let Me Cook <badaroumouwafic@gmail.com>

---
+++ src/rapport_mission.py
@@ -0,0 +1,66 @@
+from datetime import date, datetime
+import os
+
+class generer_rapport_mission():
+    
+    def __init__(self):
+        pass
+    
+    def generer_rapport_synthese(self, resultats_de_mission:dict)->None:
+        """
+            Retourne le rapport de mission effectué.
+            Args: 
+                resultats_de_mission(dict): les résultats de l'opération de déchiffrement du fichier
+            Returns:
+                str: le rapport
+        """
+        
+        equivalence=['AES-CBC-256', 'CHACHA20', 'BLOWFISH', 'AES-GCM', 'FERNET']
+        
+        try :
+            rapport= f"RAPPORT DE SYNTHESE DU {date.today().strftime("%d/%m/%y")} à {str(datetime.now().time()).split('.')[0]}\n " 
+            +  f"Mission {equivalence.index(resultats_de_mission['algorithme'].toupper()) + 1}: {resultats_de_mission['algorithme'].toupper()} \n" 
+            +  "I - Statistiques relatives à l'analyse du fichier\n"
+            + f"Fichier crypté par cet algorithme: {resultats_de_mission['fichier']}\n"
+            + f"Clé de déchiffrement identifiée: {resultats_de_mission['cle']} \n"
+            + f"Nombre de tentatives: {resultats_de_mission['tentatives']} \n"
+            + f"Temps d'exécution: {resultats_de_mission["temps_execution"]} \n"
+            +  "II - Résultats obtenus"
+            + f"Taux réussite du déchiffrement: {resultats_de_mission['taux_succes']}({resultats_de_mission['statut_succes']})\n"
+            + f"Texte déchiffré: {resultats_de_mission['texte_dechiffre']} \n"
+            
+            # Ecriture du rapport dans le fichier rapport.txt pour les affichage ultérieurs
+            with open(f"{os.path.abspath(os.curdir)}\\CryptoForensic-Python\\rapport_mission.txt", 'a') as f:
+                f.write(rapport.replace('\n', '~'))
+                f.close()
+                
+            return rapport
+        except (KeyError, ValueError):
+            print("Une erreur s'est produite.")
+        
+        return rapport
+    
+    
+    def recuperer_ancien_rapport(self, base_date:str)->list|str:
+        """
+            Récupère un/les ancien(s) rapport(s) dans le fichier rapport.txt
+            
+            Args: 
+                base_date(str): la date à laquelle le/les rapport(s) a/ont été émis
+            Returns:
+                list|str: les rapports s'ils existent.
+
+
+        """
+        rapport=[]
+        try:
+            with open(f"{os.path.abspath(os.curdir)}\\rapport.txt", 'r') as f:
+                line=f.readline()
+                while line != "" :
+                    if line.find(base_date) != -1:
+                        rapport.append(line)
+                f.close()
+                return rapport if rapport.count() > 0 else 'Aucun rapport trouvé à cette date.'
+        except FileNotFoundError:
+            print('Fichier non trouvé')
+            
