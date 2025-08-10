f7e6858fd713370eaa41049f0143b0e2abc6e1a1
mouwaficbdr <badaroumouwafic@gmail.com>
2025-08-10 17:22:20 +0100

    fix: Standardisation uniforme de tous les imports pour uniformité

--- src/analyzers/fernet_analyzer.py
+++ src/analyzers/fernet_analyzer.py
@@ -5,7 +5,7 @@ import time
 from cryptography.fernet import Fernet
 from typing import List
 
-from ..crypto_analyzer import CryptoAnalyzer
+from src.crypto_analyzer import CryptoAnalyzer
 
 class FernetAnalyzer(CryptoAnalyzer):
   """
